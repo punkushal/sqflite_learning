305009f8f7f57b6840b7a3553b024263063b6a2d
punkushal <spycymagar@gmail.com>
2026-03-31 14:02:52 +0545

    call database method to see the implementation log

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_learning/db/first_db.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await myFirstDatabase();
   runApp(const MyApp());
 }
 
